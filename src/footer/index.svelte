bffa91f40ee09b520a4ec6920a3a1452283e73cd
Ed Asriyan <github@asriyan.me>
2025-10-12 21:18:26 +0000

    Update social icons and replace IcoMoon with Fontello

--- src/footer/index.svelte
+++ src/footer/index.svelte
@@ -2,17 +2,18 @@
     import Social from './social.svelte';
 </script>
 <footer class="footer">
+    <Social name="SimpleX" icon="simplex" href="https://smp17.simplex.im/a#4fDJYxKMGNve_os7F-y4Fjt_gc0Ar5x3JHHdfH5k2IE"/>
     <Social name="GitHub" icon="github" href="https://github.com/ed-asriyan"/>
     <Social name="LinkedIn" icon="linkedin" href="https://www.linkedin.com/in/ed-asriyan"/>
     <Social name="Facebook" icon="facebook" href="https://www.facebook.com/ed.asrian"/>
     <Social name="VK" icon="vk" href="https://vk.com/id84279691"/>
     <Social name="Instagram" icon="instagram" href="https://www.instagram.com/ed_asriyan"/>
     <Social name="Telegram" icon="telegram" href="https://t.me/ed_asriyan"/>
-    <Social name="Spotify" icon="spotify" href="https://open.spotify.com/user/31ghpn3sdqez7c6mbsi556bf5ovy?si=fd7a81b6514e4a61"/>
+    <Social name="Email" icon="email" href="mailto:contact.website@asriyan.me" title="Email Ed Asriyan"/>
 
     <div class="marks">
         <a href="https://jonphillips.ca" target="_blank">Template</a>,
-        <a href="https://icomoon.io/" target="_blank">IcoMoon</a>,
+        <a href="https://fontello.com/" target="_blank">Fontello</a>,
         <a href="https://threejs.org" target="_blank">Background Spheres</a>
     </div>
 </footer>
--- src/footer/social.svelte
+++ src/footer/social.svelte
@@ -1,45 +1,95 @@
 <script lang="ts" module>
-    type Icon = "github" | "vk" | "linkedin";
+    type Icon = "github" | "vk" | "linkedin" | "facebook" | "simplex" | "instagram" | "email" | "telegram";
 </script>
 
 <script lang="ts">
-   interface Props {
-      icon: Icon;
-      href: string;
-      name: string;
-   }
+    interface Props {
+       icon: Icon;
+       href: string;
+       name: string;
+       round: boolean;
+       title?: string;
+    }
 
-   let { icon, href, name }: Props = $props();
+    let { icon, href, name, title }: Props = $props();
 
-    let title = $derived(`Ed Asriyan at ${name}`);
+    let hint = $derived(title || `Ed Asriyan at ${name}`);
     let classes = $derived(['social', icon].join(' '));
+
+    const map: Record<Icon, { symbol: string }> = {
+        'facebook': {
+            symbol: '&#xf09a;',
+        },
+        'simplex': {
+            symbol: '&#xe800;',
+        },
+        'email': {
+            symbol: '&#xe801;',
+        },
+        'linkedin': {
+            symbol: '&#xe802;',
+        },
+        'instagram': {
+            symbol: '&#xf16d;',
+        },
+        'vk': {
+            symbol: '&#xf189;',
+        },
+        'telegram': {
+            symbol: '&#xe803;',
+        },
+        'github': {
+            symbol: '&#xf113;',
+        },
+    };
+
+    const onClick = () => {
+        window.open(href, '_blank');
+    };
 </script>
 
-<a class={classes}
-   href={href}
-   title={title}
-   target="_blank"></a>
+<button title={hint} onclick={onClick} type="button">
+    <svg class="icon" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <mask id={`symbol-mask-${icon}`}>
+                <rect width="100" height="100" fill="white"  />
+                <text
+                    class={classes}
+                    x="50"
+                    y="50"
+                    fill="black"
+                    font-size="50"
+                    text-anchor="middle"
+                    dominant-baseline="central"
+                >
+                    {@html map[icon]?.symbol}
+                </text>
+            </mask>
+        </defs>
+
+        <circle cx="50" cy="50" r="50" class={classes} mask={`url(#symbol-mask-${icon})`} />
+    </svg>
+</button>
 
 <style>
     @font-face {
-        font-family: 'icomoon';
-        src: url('/icomoon.ttf') format('truetype');
+        font-family: 'icons';
+        src: url('/fontello.ttf') format('truetype');
+    }
+
+    button {
+        background: none;
+        border: none;
+        padding: 0;
+        margin: 0;
     }
 
     .social {
-        margin: 0 0.5rem;
-        color: black;
-        font-size: 40px;
+        fill: black;
         transition: all 0.5s ease;
-
-        font-family: 'icomoon';
+        cursor: pointer;
+        font-family: 'icons';
         speak: none;
-        font-style: normal;
-        font-weight: normal;
-        font-variant: normal;
-        text-transform: none;
-        line-height: 1;
-        text-decoration: none;
 
         &:hover {
             transition: all 0.5s ease;
@@ -48,83 +98,75 @@
 
     .github {
         &:hover {
-            color: #24292e;
-        }
-
-        &:before {
-            content: "\e900";
+            fill: #24292e;
         }
     }
 
     .telegram {
         &:hover {
-            color: #179cde;
-        }
-
-        &:before {
-            content: "\e902";
+            fill: #179cde;
         }
     }
 
     .vk {
         &:hover {
-            color: #0077ff;
-        }
-
-        &:before {
-            content: "\e901";
+            fill: #0077ff;
         }
     }
 
     .facebook {
         &:hover {
-            color: #1877f2;
-        }
-
-        &:before {
-            content: "\e905";
+            fill: #1877f2;
         }
     }
 
     .instagram {
         &:hover {
-            color: #db3670;
-        }
-
-        &:before {
-            content: "\e904";
+            fill: #db3670;
         }
     }
 
     .linkedin {
         &:hover {
-            color: rgb(41, 119, 201);
-        }
-
-        &:before {
-            content: "\e903";
+            fill: rgb(41, 119, 201);
         }
     }
 
     .spotify {
         &:hover {
-            color: rgb(30, 215, 96);
-        }
-
-        &:before {
-            content: "\e906";
+            fill: rgb(30, 215, 96);
         }
     }
 
+    .simplex {
+        &:hover {
+            fill: #01c4ff;
+        }
+    }
+
+    .email {
+        &:hover {
+            fill: #eab738;
+        }
+    }
+
+    .icon {
+        height: 45px;
+        width: 45px;
+        margin: 0.5rem;
+    }
+
     @media all and (max-width: 600px) {
-        .social {
-            font-size: 37px;
+        .icon {
+            height: 37px;
+            width: 37px;
         }
     }
 
     @media all and (max-width: 480px) {
-        .social {
-            font-size: 34px;
+        .icon {
+            height: 34px;
+            width: 34px;
         }
     }
 </style>
