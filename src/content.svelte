03de15868221cd16fd08c3c58fd2497ee62acd3e
Ed Asriyan <github@asriyan.me>
2025-10-24 22:16:19 +0000

    Add social media icons component and update footer to use it

--- src/content.svelte
+++ src/content.svelte
@@ -2,17 +2,30 @@
 <!-- https://jonphillips.ca !-->
 <!-- потом когда-нибудь сделаю сам (никогда) !-->
 
+<script lang="ts">
+    import Social from './social.svelte';
+</script>
+
 <main class="container">
     <section class="content">
         <img class="avatar" src="https://avatars0.githubusercontent.com/u/7848847" crossorigin="anonymous">
         <h1 class="name">Ed Asriyan</h1>
-        <a class="email" href="mailto:contact.website@asriyan.me" title="Email Ed Asriyan">Contact</a>
+         <div align="center" class="social-container">
+            <Social name="SimpleX" icon="simplex" href="https://smp17.simplex.im/a#4fDJYxKMGNve_os7F-y4Fjt_gc0Ar5x3JHHdfH5k2IE"/>
+            <Social name="GitHub" icon="github" href="https://github.com/ed-asriyan"/>
+            <Social name="LinkedIn" icon="linkedin" href="https://www.linkedin.com/in/ed-asriyan"/>
+            <Social name="Facebook" icon="facebook" href="https://www.facebook.com/ed.asrian"/>
+            <Social name="VK" icon="vk" href="https://vk.com/id84279691"/>
+            <Social name="Instagram" icon="instagram" href="https://www.instagram.com/ed_asriyan"/>
+            <Social name="Telegram" icon="telegram" href="https://t.me/ed_asriyan"/>
+            <Social name="Email" icon="email" href="mailto:contact.website@asriyan.me" title="Email Ed Asriyan"/>
+        </div>
     </section>
 </main>
 
 <style>
     .container {
-        margin-top: -75px;
+        margin-top: -2rem;
         position: absolute;
         height: 100vh;
         width: 100vw;
@@ -54,50 +67,7 @@
         }
     }
 
-    .email {
-        text-align: center;
-        display: inline-block;
-        margin-top: 5px;
-        width: auto;
-        font-weight: 500;
-        background: rgba(86, 80, 78, 0.2);
-        backdrop-filter: blur(8px);
-        border-radius: 50px;
-        border: solid 1px black;
-        padding: 0.75rem 1.5rem;
-        font-size: 0.9rem;
-        letter-spacing: 0.25rem;
-        text-transform: uppercase;
-        color: black;
-        transition: all 0.5s ease;
-        text-decoration: none;
-
-        &:hover {
-            background-color: black;
-            color: white;
-        }
-    }
-
-    @media (max-width: 740px) {
-        .email {
-            display: block;
-            padding: 1.35rem 1.5rem;
-            font-size: 1.4rem;
-            margin-top: 2rem;
-        }
-    }
-
-    @media (max-width: 600px) {
-        .email {
-            margin: 1.45rem 1.5rem;
-            font-size: 1.5rem;
-        }
-    }
-
-    @media (max-width: 480px) {
-        .email {
-            margin: 1.55rem 1.5rem;
-            font-size: 1.6rem;
-        }
+    .social-container {
+        margin-top: 1.5rem;
     }
 </style>
--- src/footer/index.svelte
+++ src/footer/index.svelte
@@ -1,16 +1,4 @@
-<script lang="ts">
-    import Social from './social.svelte';
-</script>
 <footer class="footer">
-    <Social name="SimpleX" icon="simplex" href="https://smp17.simplex.im/a#4fDJYxKMGNve_os7F-y4Fjt_gc0Ar5x3JHHdfH5k2IE"/>
-    <Social name="GitHub" icon="github" href="https://github.com/ed-asriyan"/>
-    <Social name="LinkedIn" icon="linkedin" href="https://www.linkedin.com/in/ed-asriyan"/>
-    <Social name="Facebook" icon="facebook" href="https://www.facebook.com/ed.asrian"/>
-    <Social name="VK" icon="vk" href="https://vk.com/id84279691"/>
-    <Social name="Instagram" icon="instagram" href="https://www.instagram.com/ed_asriyan"/>
-    <Social name="Telegram" icon="telegram" href="https://t.me/ed_asriyan"/>
-    <Social name="Email" icon="email" href="mailto:contact.website@asriyan.me" title="Email Ed Asriyan"/>
-
     <div class="marks">
         <a href="https://jonphillips.ca" target="_blank">Template</a>,
         <a href="https://fontello.com/" target="_blank">Fontello</a>,
